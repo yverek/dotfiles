218a2e758d3fdc8a636879f1b3a201d7e7b6707e
Mauro Dattilo <mau.dattilo@gmail.com>
2019-04-05 15:46:57 +0200

    Add config file to store all settings

---
+++ software/config.py
@@ -0,0 +1,22 @@
+# This file contains every settings you can modify to fit your needs
+
+SOURCES_LIST = """
+
+"""
+
+DRIVERS = 'amd64-microcode firmware-amd-graphics xserver-xorg-video-radeon'
+
+SOFTWARE = "htop strace lshw qt4-qtconfig acpi acpi-support aspell-it "
+SOFTWARE += "hunspell-it mythes-it menulibre p7zip-rar redshift-gtk "
+SOFTWARE += "plank lm-sensors hddtemp"
+
+#####
+
+#####
+# Edit the following if updates
+#####
+
+MEGA_LINK = 'https://mega.nz/linux/MEGAsync/Debian_9.0/amd64/megasync-Debian_9.0_amd64.deb'
+
+
+#####
--- software/utils.py
+++ software/utils.py
@@ -3,28 +3,6 @@ from pathlib import Path
 import subprocess
 import sys
 
-#####
-# Edit the following section to fit your needs
-#####
-
-DRIVERS = 'amd64-microcode firmware-amd-graphics xserver-xorg-video-radeon'
-
-SOFTWARE = "htop strace lshw qt4-qtconfig acpi acpi-support aspell-it "
-SOFTWARE += "hunspell-it mythes-it menulibre p7zip-rar redshift-gtk "
-SOFTWARE += "plank lm-sensors hddtemp"
-
-#####
-
-#####
-# Edit the following if updates
-#####
-
-MEGA_LINK = 'https://mega.nz/linux/MEGAsync/Debian_9.0/amd64/megasync-Debian_9.0_amd64.deb'
-
-
-#####
-
-
 
 def run_command(command):
     try:
