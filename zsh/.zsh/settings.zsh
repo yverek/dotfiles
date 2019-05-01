46f7c5c78a1fd3124b755e25ebc65084a6fe0dc2
Mauro Dattilo <mau.dattilo@gmail.com>
2019-05-01 18:43:15 +0200

    Removed pyenv path settings

--- zsh/.zsh/settings.zsh
+++ zsh/.zsh/settings.zsh
@@ -1,7 +1,5 @@
 # PyEnv Settings
 if which pyenv > /dev/null; then
-    export PYENV_ROOT="$HOME/.pyenv"
-    export PATH="$PYENV_ROOT/bin:$PATH"
     eval "$(pyenv init -)"
     eval "$(pyenv virtualenv-init -)"
 fi
