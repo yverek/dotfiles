9579c5c207b66345367bfcd1cecc854d121d9fb1
Mauro Dattilo <mau.dattilo@gmail.com>
2019-05-02 18:35:17 +0200

    Replaced which with command in settings.zsh

--- zsh/.zsh/settings.zsh
+++ zsh/.zsh/settings.zsh
@@ -1,5 +1,5 @@
 # PyEnv Settings
-if which pyenv > /dev/null; then
+if command -v pyenv > /dev/null; then
     eval "$(pyenv init -)"
     eval "$(pyenv virtualenv-init -)"
 fi
@@ -11,8 +11,9 @@ export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=6'
 export ZSH_PLUGINS_ALIAS_TIPS_TEXT='ﯦ '
 
 # Neofetch
-if which pyenv > /dev/null && [[ "$(pyenv version-name)" == "system" ]]; then
-    if which neofetch > /dev/null; then
+# This check prevents Neofetch to run if a venv is activated
+if command -v pyenv > /dev/null && [[ "$(pyenv version-name)" == "system" ]]; then
+    if command -v neofetch > /dev/null; then
         neofetch;
     fi
 fi
