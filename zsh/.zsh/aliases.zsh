c036552f0e2ba7f928ca9005d993432a4260fabc
Mauro Dattilo <mau.dattilo@gmail.com>
2019-05-06 17:05:42 +0200

    Added nvim alias

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -18,6 +18,11 @@ function take() {
   mkdir -p $@ && cd ${@:$#}
 }
 
+# Haters gonna hate
+if command -v nvim &> /dev/null; then
+    alias vim='nvim'
+fi
+
 # List directory contents
 if command -v exa &> /dev/null; then
     alias l='exa -Fghl --group-directories-first'
