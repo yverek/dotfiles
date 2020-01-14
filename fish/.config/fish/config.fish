bec94ab911bbe6cb82e74b3dca2cf58a5a16bf23
Mauro Dattilo <mau.dattilo@gmail.com>
2020-01-14 10:24:55 +0100

    Add starship prompt to fish shell

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -9,3 +9,6 @@ set -gx PATH $PYENV_ROOT/bin $PATH
 status --is-interactive; and source (pyenv init - | psub)
 
 # i'll wait powerlevel9k for full transfer to fish
+
+# starship
+starship init fish | source
