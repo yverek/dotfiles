a7aad6111ff5606c13e87f7fefdd6857faba774b
Mauro Dattilo <mau.dattilo@gmail.com>
2020-01-18 14:35:28 +0100

    Add PATH functions

--- fish/.config/fish/functions.fish
+++ fish/.config/fish/functions.fish
@@ -1,10 +1,35 @@
-# General
-function ppath
-    printf "%s\n" $PATH  # print $PATH line by line
+# PATH functions
+function path_append -d 'Append entries to PATH'
+  for entry in $argv
+    set i (contains -i $entry $PATH); and set -e PATH[$i]
+    test -e $entry; and set -a PATH $entry
+  end
 end
 
-# Create a directory and set CWD
-function take
+function path_dedupe -d 'Deduplicate PATH (prefers first entry)'
+  set PATH (string escape $PATH | awk '!seen[$0]++')
+end
+
+function path_list -d 'Lists elements in PATH (newline seperated)'
+  string escape $PATH
+end
+
+function path_prepend -d 'Prepend entries to PATH'
+  for entry in $argv[-1..1]
+    set i (contains -i $entry $PATH); and set -e PATH[$i]
+    test -e $entry; and set -p PATH $entry
+  end
+end
+
+function path_remove -d 'Remove entries from PATH'
+  for entry in $argv
+    while set i (contains -i $entry $PATH)
+      set -e PATH[$i]
+    end
+  end
+end
+
+function take -d 'Create a directory and set CWD'
     command mkdir $argv
     if test $status = 0
         switch $argv[(count $argv)]
