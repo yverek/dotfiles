4d0377ad416ba330f6ab6d044183aa22df24c808
Mauro Dattilo <mau.dattilo@gmail.com>
2020-01-19 11:38:30 +0100

    Refactor updating functions

--- fish/.config/fish/functions/functions.fish
+++ fish/.config/fish/functions/functions.fish
@@ -1,29 +1,3 @@
-#--------------------#
-# Updating functions #
-#--------------------#
-function update_arch -d 'Update Arch (pacman and yay)'
-    sudo pacman -Syu
-    yay -Syu
-end
-
-function update_vimplug -d 'Update vimplug with its plugins'
-    nvim +PlugUpdate +sleep 1000m +qall
-end
-
-function update_pyenv -d 'Update pyenv with its plugins'
-    pyenv update
-end
-
-function update_system -d 'Update everything'
-    update_arch
-    sleep 1
-    update_pyenv
-    sleep 1
-    #update_zplug
-    sleep 1
-    update_vimplug
-end
-
 #-------------------#
 # General functions #
 #-------------------#
---
+++ fish/.config/fish/functions/updates/update_vimplug.fish
@@ -0,0 +1,3 @@
+function update_vimplug -d 'Update vimplug with its plugins'
+    nvim +PlugUpdate +sleep 1000m +qall
+end
