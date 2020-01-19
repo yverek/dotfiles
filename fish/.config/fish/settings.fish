d62ddc9a87c889efe5285dbf90d6391eb7ec27b4
Mauro Dattilo <mau.dattilo@gmail.com>
2020-01-19 11:45:20 +0100

    Add kitty as default terminal emulator

--- fish/.config/fish/settings.fish
+++ fish/.config/fish/settings.fish
@@ -7,6 +7,10 @@ if command --search firefox > /dev/null
     set -gx BROWSER firefox
 end
 
+if command --search kitty > /dev/null
+    set -gx TERMINAL kitty
+end
+
 # pyenv
 set -gx PYENV_ROOT $HOME/.pyenv
 
