45484319eb409a67eb5b27691e39b0ae5e5227f6
Mauro Dattilo <mau.dattilo@gmail.com>
2021-11-28 14:19:31 +0100

    Add autoremove command to clean_ubuntu function

--- fish/.config/fish/functions/generals/clean_ubuntu.fish
+++ fish/.config/fish/functions/generals/clean_ubuntu.fish
@@ -2,7 +2,12 @@ function clean_ubuntu -d 'Clean Ubuntu installation deleting unneeded packages a
     # getting amount of space we are going to clean
     set -l FREE_SPACE (string trim (string split '/' (sudo du -sh /var/cache/apt/archives))[1])
     print_warning "We are going to delete .deb packages for a total of $FREE_SPACE!"
+    # clear out the local repository of retrieved package files
     sudo apt clean
+    sleep 1
+    # remove packages that were automatically installed to satisfy dependencies
+    # for other packages and are now no longer needed
+    sudo apt-get autoremove
     sleep 1
     print_success "Done"
 end
